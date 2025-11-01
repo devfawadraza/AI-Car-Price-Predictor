4543f51626de547a6bbbeaaa699ebc62c67bb06d
devfawadraza <fawadraza71@gmail.com>
2025-11-01 05:45:54 -0700

    new changes

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import pandas as pd
 df = pd.read_csv("data/file.csv")
 
 print(df.head(3))
+
+# print(df.info())
+
+# print(df.isnull().sum())
+
+# print(df[df['price'] == 0])
+
+# cols = ['price', 'model', 'mileage', 'engine_capacity', 'vehicle_age']
+# for col in cols:
+#     print(f"\n Rows where {col} == 0")
+#     print (df[df[col] <=10])
+
+
+print("\n Description of the columns:")
+print(df.describe())
